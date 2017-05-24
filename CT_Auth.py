92d728f477a1d7a49288f4561ef2688bef792f08
leeleegabriel <lgv0001@auburn.edu>
2017-05-24 15:34:22 -0500

    Wii!

--- CT_Auth.py
+++ CT_Auth.py
@@ -2,10 +2,9 @@ import sqlite3
 from urllib2 import Request, urlopen, URLError
 
 def main():
-	conn = sqlite3.connect('CellTowers.db')
-	c1= conn.cursor()
-	c1.execute('SELECT * FROM DetectorData')
-
+    conn = sqlite3.connect('CellTowers.db')
+    c1= conn.cursor()
+    c1.execute('SELECT * FROM DetectorData')
 
     c2 = conn.cursor() 
     # Creates table to store rogue cell tower's data
@@ -13,30 +12,27 @@ def main():
     c2.commit()
 
     # Iterates through DetectorData table
-	for row in c1:
-		cellid = row[4]
-		mcc = row[5]
-		mnc = row[6]
-		lac = row[7]
+    for row in c1:
+        cellid = row[4]
+        mcc = row[5]
+        mnc = row[6]
+        ac = row[7]
 		
 		# Makes API request
-		request_str = 'https://api.mylnikov.org/geolocation/cell?v=1.1&data=open&mcc=%s&mnc=%s&lac=%s&cellid=%s' % (mcc, mnc, lac, cellid)
-		request = Request(request_str)
-		try:
-			response = urlopen(request)
-			check = response.read()
+        request_str = 'https://api.mylnikov.org/geolocation/cell?v=1.1&data=open&mcc=%s&mnc=%s&lac=%s&cellid=%s' % (mcc, mnc, lac, cellid)
+        request = Request(request_str)
+        try:
+            response = urlopen(request)
+            check = response.read()
 			# Gets code from API response
 			# 200 = Tower is in DB and 404 = Tower is not found
-			code = int(check[14:18])
-			if(code == 404) {
+            code = int(check[14:18])
+            if(code == 404) :
 				# adds row to Rogue Cell Tower DB
-				c2.execute('INSERT INTO RogueData(t, arfcn, rxl, bsic, Cell_ID, MCC, MNC, LAC, lat, lon, satellites, gps_quality, altitude, altitude_units) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', row)
-			}
-		except URLError, e:
-    		print 'URL Error: ', e
-    		quit()
-
+                c2.execute('INSERT INTO RogueData(t, arfcn, rxl, bsic, Cell_ID, MCC, MNC, LAC, lat, lon, satellites, gps_quality, altitude, altitude_units) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', row)
+        except URLError, e:
+            print 'URL Error: ', e
+            quit()
     conn.close()
-    
 if __name__ == "__main__":
-	main()
+    main()
