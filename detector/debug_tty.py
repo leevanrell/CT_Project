a106815092415264cfcae4194e92cb6ef39132f5
leeleegabriel <lgv0001@auburn.edu>
2019-05-25 23:11:47 -0500

    fixed tty

--- detector/debug_tty.py
+++ detector/debug_tty.py
@@ -1,9 +1,10 @@
 import serial
+from time import sleep
 
 
 SIM_TTY = '/dev/ttyUSB0' 
 try:
-    Serial = serial.Serial(port=SIM_TTY, baudrate=115200, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS, timeout=0)
+    Serial = serial.Serial(port=SIM_TTY, baudrate=9600, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS, timeout=0)
     Serial.write(b'AT\r\n')
     sleep(1)
     for i in range(0, 5):
